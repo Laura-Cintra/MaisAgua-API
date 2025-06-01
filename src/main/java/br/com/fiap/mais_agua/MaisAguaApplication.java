7e690b00cd29726e7dd6fd19cf4ef733cdb70f1f
MariaEdPaixao <mariaeduardaalvesdapaixao0807@gmail.com>
2025-06-01 09:08:06 -0300

    first commit

---
+++ src/main/java/br/com/fiap/mais_agua/MaisAguaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.mais_agua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisAguaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaisAguaApplication.class, args);
+	}
+
+}
