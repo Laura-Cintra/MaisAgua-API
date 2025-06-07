d005fc3e50a6c5b0c202b830e871ed15585a8525
MariaEdPaixao <mariaeduardaalvesdapaixao0807@gmail.com>
2025-06-07 00:16:33 -0300

    fix: cors

--- src/main/java/br/com/fiap/mais_agua/config/CorsConfig.java
+++ src/main/java/br/com/fiap/mais_agua/config/CorsConfig.java
@@ -10,8 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST","DELETE", "PUT")
-                .allowedHeaders("*");
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
+
